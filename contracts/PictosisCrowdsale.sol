5d9314ad64e14d63e7ebec1d8406288ed5189930
Richard Ramos <info@richardramos.me>
2019-05-05 14:21:15 -0400

    feat: allow claiming sent by mistake tokens in crowdsale

--- contracts/PictosisCrowdsale.sol
+++ contracts/PictosisCrowdsale.sol
@@ -129,4 +129,22 @@ contract PictosisCrowdsale is CappedCrowdsale, MintedCrowdsale, TimedCrowdsale, 
         super._updatePurchasingState(beneficiary, weiAmount);
         _contributions[beneficiary] = _contributions[beneficiary].add(weiAmount);
     }
+
+    /// @notice This method can be used by the minter to extract mistakenly
+    ///  sent tokens to this contract.
+    /// @param _token The address of the token contract that you want to recover
+    ///  set to 0x0000...0000 in case you want to extract ether.
+    function claimTokens(address _token) public onlyOwner {
+        if (_token == address(0)) {
+            msg.sender.transfer(address(this).balance);
+            return;
+        }
+
+        ERC20 token = ERC20(_token);
+        uint256 balance = token.balanceOf(address(this));
+        token.transfer(msg.sender, balance);
+        emit ClaimedTokens(_token, msg.sender, balance);
+    }
+
+    event ClaimedTokens(address indexed _token, address indexed _controller, uint256 _amount);
 }
